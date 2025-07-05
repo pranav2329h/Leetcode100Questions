a483b095e86ce968fb5b7fa0606a6809358ec7e1
Pranav Hydrabade <pranav2329h@gmail.com>
2025-07-05 21:21:57 +0530

    day5

---
+++ question5.py
@@ -0,0 +1,17 @@
+#question no 3
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_index = {}
+        max_length = 0
+        start = 0
+        for end in range(len(s)):
+            if s[end] in char_index and char_index[s[end]] >= start:
+                start = char_index[s[end]] + 1
+            char_index[s[end]] = end
+            max_length = max(max_length, end - start + 1)
+        return max_length
+        
