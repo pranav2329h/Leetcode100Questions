161b801f94d369fc4040f999c6b2352536fd876b
Pranav Hydrabade <pranav2329h@gmail.com>
2025-07-07 21:27:18 +0530

    day 6

--- question4.py
+++ question4.py
@@ -1,15 +1,20 @@
-class Solution:
-    def tribonacci(self, n: int) -> int:
-        if n == 0:
+#1137. N-th Tribonacci Number
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==0:
             return 0
-        elif n == 1 or n == 2:
+        elif n==1 or n==2:
             return 1
-        ft = 0
-        st = 1
-        tt = 1
-        for i in range(1, n + 1):
-            fot = ft + st + tt
-            ft = st
-            st = tt
-            tt = fot
-        return ft
+        else:
+            ft=0
+            st=1
+            tt=1
+            i=1
+            for i in range(n):
+                fort=ft+st+tt
+                ft,st,tt=st,tt,fort
+            return ft
--- question5.py
+++ question5.py
@@ -1,17 +1,15 @@
-#question no 3
+#1518. Water Bottles
 class Solution(object):
-    def lengthOfLongestSubstring(self, s):
+    def numWaterBottles(self, numBottles, numExchange):
         """
-        :type s: str
+        :type numBottles: int
+        :type numExchange: int
         :rtype: int
         """
-        char_index = {}
-        max_length = 0
-        start = 0
-        for end in range(len(s)):
-            if s[end] in char_index and char_index[s[end]] >= start:
-                start = char_index[s[end]] + 1
-            char_index[s[end]] = end
-            max_length = max(max_length, end - start + 1)
-        return max_length
-        
+        ans=numBottles
+        while numBottles>=numExchange:
+            newBottles=numBottles // numExchange
+            remBottles=numBottles % numExchange
+            ans+=newBottles
+            numBottles=newBottles+remBottles
+        return ans
